f72b95c7fe31f64c156e942dce355da2952f2d09
shun308 <takahashi0903shun@gmail.com>
2019-12-10 14:19:52 +0900

    add

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,6 +8,7 @@ class GroupsController < ApplicationController
     @group = Group.new
     @group.users << current_user
   end
+
   def create
     @group = Group.new(group_params)
     if @group.save
